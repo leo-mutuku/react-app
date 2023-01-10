e8cdd62d194950ada304212046d570bad96b64e5
Stanikzai <wstanikzai@afiat.org>
2023-01-10 08:18:12 +0430

    First Assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import * as React from 'react'
 
+//const Lists =['Ahmad','Aziz','Wasi'];
 
+const list = [
+  {id:1,title:"Assignment 1"},
+  {id:2,title:"Assignment 2"},
+  {id:3,title:"Assignment 3"},
+];
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello World</h1>
-    </div>
-  );
+    return (
+      <div>
+      <h1>TodoList</h1>
+      <ul>
+        {/* {list.map((index) => {
+                <li id={index.id}> {index.title} </li>
+                })} */}
+        {list.map(function(l){
+          return <li key={l.id}>{l.title}</li>
+        })}
+      </ul>
+      </div>
+    );
 }
+
+
 
 export default App;
